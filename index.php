534fef465fa81fbb223e8f5225ab8aae5a000de4
zebse <91735440+zebse@users.noreply.github.com>
2022-12-06 16:09:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Website using PHP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel ="stylesheet" href ="css/style.css">
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Gateway</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+    </ul>
+    
+    </div>
+  </div>
+</nav>
+<!-- Upper part -->
+<section class="bg-dark text-light p-5 text-center text-sm-satrt">
+  <div class="container-lg">
+    <div class="d-lg-flex align-items-center justify-content-between">
+      <div>
+        <h1>Advance Your <span class="text-warning">
+          Skills and Knowledge
+        </span></h1>
+        <p class="lead my-4">
+          A tutorial, in education, is a method of transferring knowledge and may be used as a part of a learning process. 
+          More interactive and specific than a book or a lecture,a tutorial seeks to teach by example and supply the information to complete a certain task.
+        </p>
+        <button class="btn btn-primary btn-lg">Start now</button>
+      </div>
+      <div>
+        <img class="img-fluid w-50" src="images/image2.svg" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
